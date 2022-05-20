77333ddb8ec1911defbfc4c199d72082e578441c
Olivia Wen-Mei Lang <oliviawlang@gmail.com>
2022-05-20 16:15:50 -0400

    add scripts to make M0X/H0X Mittal gene classes
    The scripts for creating the gene classes in this publication are included in this commit. They are used as references for the bulk processing.

---
+++ 02_Create_Gene_Classes/1_tally_occupancies.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# This script builds the inital reference files and calculates occupancies used to make the Mittal 2022 gene classes
+
+module load gcc
+module load samtools
+module load anaconda3
+source activate mittal
+
+WRK=/path/to/2022-Mittal_SAGA/02_Create_Gene_Classes
+BIN=$WRK/../bin
+FDIR=$WRK/NormalizationFactors/
+CONTROL=$WRK/../data/BAM/masterNoTag_20180928.bam
+BLACKLIST=$WRK/../data/ChexMix_Peak_Filter_List_190612.bed
+REFPT=$WRK/../data/RefPT-YEP/
+TEMP=$WRK/tmp
+
+# Script shortcuts
+SCRIPTMANAGER=$BIN/ScriptManager-v0.13.jar
+SORT=$BIN/sort_BED_by_Score_v2.pl
+SUM=$BIN/sum_Row_CDT.pl
+UPDATES=$BIN/update_BED_score_with_TAB_score.pl
+
+cd $WRK
+[ -d $TEMP ] || mkdir $TEMP
+[ -d $FDIR ] || mkdir $FDIR
+
+# Download ScriptManager
+if [ -f $SCRIPTMANAGER ]; then
+	wget https://github.com/CEGRcode/scriptmanager/releases/download/v0.13/ScriptManager-v0.13.jar
+	mv ScriptManager-v0.13.jar $SCRIPTMANAGER
+fi
+
+##--------Calculate Normalization Values--------
+for BAMFILE in "data/BAM/11846_Taf2_i5006_BY4741_-_XO_FilteredBAM.bam" \
+ "data/BAM/12275_Sua7_i5006_BY4741_-_XO_FilteredBAM.bam" \
+ "data/BAM/26344_Sua7_i5006_BY4741_-_XO_FilteredBAM.bam" \
+ "data/BAM/11960_Spt7_i5006_BY4741_-_XO_FilteredBAM.bam" \
+ "data/BAM/20115_Spt7_i5006_BY4741_-_XO_FilteredBAM.bam" \
+ "data/BAM/28736_Taf2_i5006_BY4741_-_XO_FilteredBAM.bam";
+do
+	[ -f $BAMFILE.bai ] || samtools index $BAMFILE
+	BAM=`basename $BAMFILE ".bam"`
+	java -jar $SCRIPTMANAGER read-analysis scaling-factor $BAMFILE --ncis -c $CONTROL -f $BLACKLIST -w 500 -o $FDIR/$BAM
+done
+
+
+##--------Pileup occupancy scores for Spt7, Sua7, and Taf2--------
+
+count_occupancy () {
+	REFFILE=$1
+	BAMFILE=$2
+	OCCUPANCY=$3
+	
+	BAM=`basename $BAMFILE ".bam"`
+	REF=`basename $REFFILE ".bed"`
+	BED=$REF\_200bp
+	BASE=$TEMP/$BAM\_$BED\_read1
+
+	cd $TEMP
+	echo "EXPAND"
+	# Expand BED 200bp from center
+	java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 200 $REFFILE -o $BED.bed
+	# Tag Pileup
+	java -jar $SCRIPTMANAGER read-analysis tag-pileup $BED.bed $BAMFILE -M $BASE
+	
+	echo "MERGE"
+	# Merge offset slices from anti and sense
+	paste <(cut -f1,2,16-116 $BASE\_sense.cdt) <(cut -f47-147 $BASE\_anti.cdt) > CDT
+	perl $SUM CDT TAB
+	perl $UPDATES $REFFILE TAB BED
+	perl $SORT BED desc $OCCUPANCY-TagCount.bed
+
+	# Merge normalized slices from anti and sense
+	FACTOR=`grep 'Scaling factor' $WRK/data/NormalizationFactors/$BAM\_NCISb_ScalingFactors.out | awk -F" " '{print $3}'`
+	java -jar $SCRIPTMANAGER read-analysis scale-matrix CDT -s $FACTOR -o NORMALIZED
+	perl $SUM NORMALIZED NTAB
+	perl $UPDATES $REFFILE NTAB NBED
+	perl $SORT NBED desc $OCCUPANCY-NormalizedCount.bed
+
+	# Clean-up
+	#rm CDT NORMALIZED TAB BED
+}
+
+#===Sua7===
+for SUA7 in "$BAM/12275_Sua7_i5006_BY4741_-_YPD_WT_XO_FilteredBAM.bam" \
+ "$BAM/26344_Sua7_i5006_BY4741_-_YPD_HS6_XO_FilteredBAM.bam";
+do
+	ID=`basename $SUA7 ".bam" | awk -F"_" '{print $1}'`
+	count_occupancy $REFPT/Sua7.bed $SUA7 $TEMP/Sua7_sort-Sua7-$ID-Offset
+done
+
+#===Taf2===
+for TAF2 in "$BAM/11846_Taf2_i5006_BY4741_-_YPD_-_XO_FilteredBAM.bam" \
+ "$BAM/28736_Taf2_i5006_BY4741_-_YPD_HS6_XO_FilteredBAM.bam";
+do
+	ID=`basename $TAF2 ".bam" | awk -F"_" '{print $1}'`
+	count_occupancy $REFPT/Sua7.bed $TAF2 $TEMP/Sua7_sort-Taf2-$ID-Offset
+done
+
+#===Spt7===
+for SPT7 in "$BAM/11960_Spt7_i5006_BY4741_-_YPD_MHS_XO_FilteredBAM.bam" \
+ "$BAM/20115_Spt7_i5006_BY4741_-_YPD_HS6_XO_FilteredBAM.bam";
+do
+	ID=`basename $SPT7 ".bam" | awk -F"_" '{print $1}'`
+	count_occupancy $REFPT/STM.bed $SPT7 $TEMP/STM_sort-Spt7-$ID-Offset
+done
---
+++ 02_Create_Gene_Classes/2_build_Mittal_GeneClasses.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+# This script builds the the Mittal 2022 gene classes
+
+WRK=/path/to/2022-Mittal_2022/02_Create_Gene_Classes
+BIN=$WRK/../bin
+ROSSI=$WRK/../data/RefPT-YEP
+MITTAL=$WRK/../data/RefPT-Mittal
+TEMP=$WRK/tmp
+
+
+#===Initial Inputs===
+TSS=$ROSSI/TSS.bed
+SUA7_MHS=$TEMP/Sua7_sort-Sua7-12275-Offset-NormalizedCount.bed
+SUA7_HS=$TEMP/Sua7_sort-Sua7-26344-Offset-NormalizedCount.bed
+STM_MHS=$TEMP/STM_sort-Spt7-11960-Offset-NormalizedCount.bed
+STM_HS=$TEMP/STM_sort-Spt7-20115-Offset-NormalizedCount.bed
+TAF2_MHS=$TEMP/Sua7_sort-Taf2-11846-Offset-NormalizedCount.bed
+TAF2_HS=$TEMP/Sua7_sort-Taf2-28736-Offset-NormalizedCount.bed
+
+
+#===Script Shortcuts===
+SCRIPTMANAGER=$WRK/bin/ScriptManager-v0.13.jar
+FILTERS=$BIN/filter_BED_by_string_ColumnSelect.pl
+FILTERV=$BIN/filter_BED_by_value_ColumnSelect.pl
+FILTERL=$BIN/filter_BED_by_list_ColumnSelect.pl
+CLOSEST=$BIN/determine_closest_RefPoint_output_Both.pl
+UPDATEC=$BIN/update_BED_coord.pl
+UPDATES=$BIN/update_BED_score_with_TAB_score.pl
+RATIO=$BIN/calculate_BED_ScoreRatio.pl
+SORT=$BIN/sort_BED_by_Score_v2.pl
+SUM=$BIN/sum_Row_CDT.pl
+DEDUPLICATE=$BIN/deduplicate_BED_coord_keep_highest_score.py
+STM=$BIN/get_STM_from_Rossi_STable1.py
+
+cd $WRK
+[ -d $MITTAL ] || mkdir $MITTAL
+[ -d $MASTER ] || mkdir $MASTER
+[ -d $TEMP ] || mkdir $TEMP
+cd $TEMP
+
+
+#===Build M01 and H01 gene classes===
+
+# Filter to keep only 01_RP
+perl $FILTERS $TSS 01_RP 4 keep TSS_01-RP.bed
+
+# Get geneID of YEP class=01_RP
+cut -f4 TSS_01-RP.bed > 01-RP.tab
+
+# Filter Sua7 bedfile for YEP classes=01_RP
+perl $FILTERL $SUA7_MHS 01-RP.tab 3 keep Sua7_RP_sort-Sua7-12275.bed
+perl $FILTERL $SUA7_HS 01-RP.tab 3 keep Sua7_RP_sort-Sua7-26344.bed
+
+# Update BED file with Spt7 occupancy score
+perl $UPDATES Sua7_RP_sort-Sua7-12275.bed <(cut -f4,5 $STM_MHS) Sua7_RP_score-Spt7-11960.bed
+perl $UPDATES Sua7_RP_sort-Sua7-26344.bed <(cut -f4,5 $STM_HS) Sua7_RP_score-Spt7-20115.bed
+
+# Sort BED file by the occupancy score
+perl $SORT Sua7_RP_score-Spt7-11960.bed desc $MITTAL/Sua7_RP_sort-Spt7-11960.bed
+perl $SORT Sua7_RP_score-Spt7-20115.bed desc $MITTAL/Sua7_RP_sort-Spt7-20115.bed
+
+# Expand TFIIB/Sua7 coordinates to 1000bp
+java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1000 $MITTAL/Sua7_RP_sort-Spt7-11960.bed -o $MITTAL/Sua7_RP_sort-Spt7-11960_1000bp.bed
+java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1000 $MITTAL/Sua7_RP_sort-Spt7-20115.bed -o $MITTAL/Sua7_RP_sort-Spt7-20115_1000bp.bed
+
+#===Build M02/H02 gene classes===
+echo Build X02
+
+# Filter to remove 01_RP
+perl $FILTERL $SUA7_MHS 01-RP.tab 3 remove Sua7_STM-TFO-UNB_sort-Sua7-12275_unfiltered.bed
+perl $FILTERL $SUA7_HS 01-RP.tab 3 remove Sua7_STM-TFO-UNB_sort-Sua7-26344-unfiltered.bed
+
+# Remove blacklisted genes (YNR050C)
+grep -v 'YNR050C' Sua7_STM-TFO-UNB_sort-Sua7-12275_unfiltered.bed > Sua7_STM-TFO-UNB_sort-Sua7-12275.bed
+grep -v 'YNR050C' Sua7_STM-TFO-UNB_sort-Sua7-26344-unfiltered.bed > Sua7_STM-TFO-UNB_sort-Sua7-26344.bed
+
+# Take top 1000 genes
+head -n 1000 Sua7_STM-TFO-UNB_sort-Sua7-12275.bed > Sua7_top1000_sort-Sua7-12275.bed
+head -n 1000 Sua7_STM-TFO-UNB_sort-Sua7-26344.bed > Sua7_top1000_sort-Sua7-26344.bed
+
+# Pull Taf2 occupancy scores
+cut -f4,5 $TAF2_MHS > Taf2-11846_occupancy.tab
+cut -f4,5 $TAF2_HS > Taf2-28736_occupancy.tab
+
+# Update with Taf2 scores
+perl $UPDATES Sua7_top1000_sort-Sua7-12275.bed Taf2-11846_occupancy.tab Sua7_top1000_score-Taf2-11846.bed
+perl $UPDATES Sua7_top1000_sort-Sua7-26344.bed Taf2-28736_occupancy.tab Sua7_top1000_score-Taf2-28736.bed
+
+# Calculate Sua7/Taf2 ratio
+perl $RATIO Sua7_top1000_sort-Sua7-12275.bed Sua7_top1000_score-Taf2-11846.bed Sua7_top1000_score-Sua7Taf2-ratio-MHS.bed
+perl $RATIO Sua7_top1000_sort-Sua7-26344.bed Sua7_top1000_score-Taf2-28736.bed Sua7_top1000_score-Sua7Taf2-ratio-HS.bed
+
+# Sort BED file by the ratio score
+perl $SORT Sua7_top1000_score-Sua7Taf2-ratio-MHS.bed desc Sua7_top1000_sort-Sua7Taf2-ratio-MHS.bed
+perl $SORT Sua7_top1000_score-Sua7Taf2-ratio-HS.bed desc Sua7_top1000_sort-Sua7Taf2-ratio-HS.bed
+
+# Pull M02/H02 classes
+head -n 300 Sua7_top1000_sort-Sua7Taf2-ratio-MHS.bed > Sua7_M02_sort-Sua7Taf2-ratio.bed
+head -n 300 Sua7_top1000_sort-Sua7Taf2-ratio-HS.bed > Sua7_H02_sort-Sua7Taf2-ratio.bed
+
+# Determine class labels
+head -n 150 Sua7_M02_sort-Sua7Taf2-ratio.bed > Sua7_M02a_sort-Sua7Taf2-ratio.bed
+tail -n 150 Sua7_M02_sort-Sua7Taf2-ratio.bed > Sua7_M02b_sort-Sua7Taf2-ratio.bed
+head -n 150 Sua7_H02_sort-Sua7Taf2-ratio.bed > Sua7_H02a_sort-Sua7Taf2-ratio.bed
+tail -n 150 Sua7_H02_sort-Sua7Taf2-ratio.bed > Sua7_H02b_sort-Sua7Taf2-ratio.bed
+
+# Pull Sua7 occupancy scores
+cut -f4,5 $SUA7_MHS > Sua7-12275_occupancy.tab
+cut -f4,5 $SUA7_HS > Sua7-26344_occupancy.tab
+
+# Update with Sua7 scores
+perl $UPDATES Sua7_M02a_sort-Sua7Taf2-ratio.bed Sua7-12275_occupancy.tab Sua7_M02a_score-Sua7-12275.bed
+perl $UPDATES Sua7_M02b_sort-Sua7Taf2-ratio.bed Sua7-12275_occupancy.tab Sua7_M02b_score-Sua7-12275.bed
+perl $UPDATES Sua7_H02a_sort-Sua7Taf2-ratio.bed Sua7-26344_occupancy.tab Sua7_H02a_score-Sua7-26344.bed
+perl $UPDATES Sua7_H02b_sort-Sua7Taf2-ratio.bed Sua7-26344_occupancy.tab Sua7_H02b_score-Sua7-26344.bed
+
+
+#M02A=Sua7_M02a_sort-Sua7Taf2-ratio
+#M02B=Sua7_M02b_sort-Sua7Taf2-ratio
+#H02A=Sua7_H02a_sort-Sua7Taf2-ratio
+#H02B=Sua7_H02b_sort-Sua7Taf2-ratio
+
+M02A=$MITTAL/FEAT-Pol-II_RefPT-Sua7___SubFEAT-25C_M02a__150_SORT-Sua7occ
+M02B=$MITTAL/FEAT-Pol-II_RefPT-Sua7___SubFEAT-25C_M02b__150_SORT-Sua7occ
+H02A=$MITTAL/FEAT-Pol-II_RefPT-Sua7___SubFEAT-37C_H02a__150_SORT-Sua7occ
+H02B=$MITTAL/FEAT-Pol-II_RefPT-Sua7___SubFEAT-37C_H02b__150_SORT-Sua7occ
+
+# Sort by Sua7 occupancy
+perl $SORT Sua7_M02a_score-Sua7-12275.bed desc $M02A.bed
+perl $SORT Sua7_M02b_score-Sua7-12275.bed desc $M02B.bed
+perl $SORT Sua7_H02a_score-Sua7-26344.bed desc $H02A.bed
+perl $SORT Sua7_H02b_score-Sua7-26344.bed desc $H02B.bed
+
+# Expand TFIIB/Sua7 coordinates to 1000bp
+java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1000 $M02A.bed -o $M02A\_1000bp.bed
+java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1000 $M02B.bed -o $M02B\_1000bp.bed
+java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1000 $H02A.bed -o $H02A\_1000bp.bed
+java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1000 $H02B.bed -o $H02B\_1000bp.bed
+
+#cut -f4 tmp_makerefs/Sua7_Mittal-M02a_sort-Sua7Taf2-ratio.bed Chitvan_BED/M02a_TFIIB_1000bp.bed |sort |uniq -c |awk '{print $1}'  |sort |uniq -c
+
+REFPT_NUC=Nuc_YEPclass.bed
+
+NM02A=$MITTAL/FEAT-Pol-II_RefPT+1Nuc___SubFEAT-25C_M02a__150_SORT-Sua7occ
+NM02B=$MITTAL/FEAT-Pol-II_RefPT+1Nuc___SubFEAT-25C_M02b__150_SORT-Sua7occ
+NH02A=$MITTAL/FEAT-Pol-II_RefPT+1Nuc___SubFEAT-37C_H02a__150_SORT-Sua7occ
+NH02B=$MITTAL/FEAT-Pol-II_RefPT+1Nuc___SubFEAT-37C_H02b__150_SORT-Sua7occ
+
+# Get Nuc RefPT
+perl $UPDATEC $M02A.bed $REFPT_NUC $NM02A.bed
+perl $UPDATEC $M02B.bed $REFPT_NUC $NM02B.bed
+perl $UPDATEC $H02A.bed $REFPT_NUC $NH02A.bed
+perl $UPDATEC $H02B.bed $REFPT_NUC $NH02B.bed
+
+#  Expand BED 200bp from center
+java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 100 $NM02A.bed -o $NM02A\_1000bp.bed
+java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 100 $NM02B.bed -o $NM02B\_1000bp.bed
+java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 100 $NH02A.bed -o $NH02A\_1000bp.bed
+java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 100 $NH02B.bed -o $NH02B\_1000bp.bed
+
+
+cd ../
+
+cut -f4 tmp_makerefs/$M02A.bed Chitvan_BED/M02a_TFIIB_1000bp.bed |sort |uniq -c |wc -l 
+#169
+cut -f4 tmp_makerefs/$M02B.bed Chitvan_BED/M02b_TFIIB_1000bp.bed |sort |uniq -c |wc -l 
+#219
+cut -f4 tmp_makerefs/$H02A.bed Chitvan_BED/H02a_TFIIB_1000bp.bed |sort |uniq -c |wc -l 
+#170
+cut -f4 tmp_makerefs/$H02B.bed Chitvan_BED/H02b_TFIIB_1000bp.bed |sort |uniq -c |wc -l 
+#218
+
+
