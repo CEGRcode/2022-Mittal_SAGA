e09af01b2fb9664f7a3c5d36b1452c6b60643503
owlang <oliviawlang@gmail.com>
2022-08-11 16:58:22 -0400

    fix paths to text files with pegr ids
    this commit fixes the error in the download script using the wrong directory for the lists of sample ids.

--- 00_Download_and_preprocessing/1_download_files.sh
+++ 00_Download_and_preprocessing/1_download_files.sh
@@ -32,12 +32,12 @@ mv masterNoTag_20180928.bam data/BAM/
 samtools index data/BAM/masterNoTag_20180928.bam
 
 # Download all sequencing samples (Mittal & YEP)
-python bin/download_ALL_PEGR_py3.py -u $USER_EMAIL -g $GALAXY_API_KEY -p $PEGR_API_KEY -n tmp-galaxy -f data/sample_ids.txt
+python bin/download_ALL_PEGR_py3.py -u $USER_EMAIL -g $GALAXY_API_KEY -p $PEGR_API_KEY -n tmp-galaxy -f 00_Download_and_preprocessing/sample_ids.txt
 mv tmp-galaxy/*.bam data/BAM/
 mv tmp-galaxy/*.fastq.gz data/FASTQ/
 
 # Download all RNA samples (Mittal)
-python bin/download_ALL_PEGR_py3.py -u $USER_EMAIL -g $GALAXY_API_KEY -p $PEGR_API_KEY -n tmp-rna-galaxy -f data/pegr_ids_rna.txt
+python bin/download_ALL_PEGR_py3.py -u $USER_EMAIL -g $GALAXY_API_KEY -p $PEGR_API_KEY -n tmp-rna-galaxy -f 00_Download_and_preprocessing/pegr_ids_rna.txt
 mv tmp-rna-galaxy/*.bam data/RNA-BAM/
 mv tmp-rna-galaxy/*.fastq.gz data/RNA-FASTQ/
 
